00f41e8e23532cb222a3da7891ee174b8a3bd54d
luiz-om <luizfernando_martins@hotmail.com>
2019-01-03 13:57:01 -0300

    Erro ao incializar

---
+++ src/main/java/com/agro/curso/boot/domain/AbstractEntity.java
@@ -0,0 +1,60 @@
+package com.agro.curso.boot.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class AbstractEntity<ID extends Serializable> implements Serializable {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private ID id;
+
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity<?> other = (AbstractEntity<?>) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "id=" + id ;
+	}
+}
